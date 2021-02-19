9c79032a284c356c7fe5da9381e0b2c68fc1ef9a
MarcBaquesSabat <marc.baques@enti.cat>
2021-02-19 17:20:17 +0100

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+
+
+int main(){
+    std::vector<int> saves;
+    int my{3};
+    saves.push_back(my);
+    std::cout << "Hello world.\n";
+
+    return 0;
+}
